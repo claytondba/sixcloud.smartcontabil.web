808e766bc4aa4a49292184150123b7c293df816f
claytonoliveira <clayton@devplus.com.br>
2023-08-26 18:47:49 -0300

    login

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { LoginComponent } from "./login/login.component";
+import { PoModule } from "@po-ui/ng-components";
+import { CommonModule } from "@angular/common";
+import { LoginModule } from "./login/login.module";
+
+@NgModule({
+    declarations:[
+        
+    ],
+    imports:[
+        LoginModule
+    ],
+    exports:[
+        
+    ]
+})
+export class HomeModule { }
