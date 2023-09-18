5d0e12452b85074ca9e8e4c475da0c97abadc2a4
Clayton Oliveira <claytondba@yahoo.com.br>
2023-09-18 10:49:09 -0300

    download arquivos giss

--- src/app/home/giss/giss.component.ts
+++ src/app/home/giss/giss.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 import { PoBreadcrumb } from "@po-ui/ng-components";
 import { PoPageDynamicTableActions, PoPageDynamicTableCustomTableAction } from "@po-ui/ng-templates";
+import { GissService } from "./giss.service";
 
 @Component({
     selector: 'sixcloud-giss',
@@ -39,24 +40,24 @@ export class GissComponent {
             //action: this.onClickEmpresaDetail.bind(this),
         },
         {
-          label: 'Gua de Prestados',
-          icon: 'po-icon-download'
-          //action: this.onClickEmpresaDetail.bind(this),
+          label: 'Guia de Prestados',
+          icon: 'po-icon-download',
+          action: this.downloadGuiaPrestados.bind(this),
         },
         {
             label: 'Livro Prestados',
-            icon: 'po-icon-download'
-            //action: this.onClickEmpresaDetail.bind(this),
+            icon: 'po-icon-download',
+            action: this.downloadLivroPrestados.bind(this),
         },
         {
             label: 'Livro Tomados',
-            icon: 'po-icon-download'
-            //action: this.onClickEmpresaDetail.bind(this),
+            icon: 'po-icon-download',
+            action: this.downloadLivroTomados.bind(this),
         },
         {
             label: 'Guias/Certificados',
-            icon: 'po-icon-download'
-            //action: this.onClickEmpresaDetail.bind(this),
+            icon: 'po-icon-download',
+            action: this.downloadGuiaTomados.bind(this),
         },
         {
             label: 'Aceite de Notas',
@@ -66,6 +67,60 @@ export class GissComponent {
         
       ];
 
+      constructor(private gissService: GissService ) { }
 
+      downloadLivroPrestados(giss: any)
+      {
+        console.log(giss.id);
+            this.gissService.getDocument(giss.id, 'livro-prestados').subscribe(file => {
+                this.downloadFile(giss.cnpj + "-" + giss.competencia + "-livro-prestados.csv", file);
+            });
+      }
+
+      downloadGuiaPrestados(giss: any)
+      {
+        console.log(giss.id);
+            this.gissService.getDocument(giss.id, 'guia-prestados').subscribe(file => {
+                this.downloadFile(giss.cnpj + "-" + giss.competencia + "-guia-prestados.rar", file);
+            });
+      }
+
+      downloadGuiaTomados(giss: any)
+      {
+        console.log(giss.id);
+            this.gissService.getDocument(giss.id, 'guia-tomados').subscribe(file => {
+                this.downloadFile(giss.cnpj + "-" + giss.competencia + "-guia_tomados.rar", file);
+            });
+      }
+
+      downloadLivroTomados(giss: any)
+      {
+        console.log(giss.id);
+            this.gissService.getDocument(giss.id, 'livro-tomados').subscribe(file => {
+                this.downloadFile(giss.cnpj + "-" + giss.competencia + "-livro_tomados.rar", file);
+            });
+      }
+
+      downloadAceiteTomados(giss: any)
+      {
+        console.log(giss.id);
+            this.gissService.getDocument(giss.id, 'rel_notas_emit').subscribe(file => {
+                this.downloadFile(giss.cnpj + "-" + giss.competencia + "-rel_notas_emit.xls", file);
+            });
+      }
+
+      downloadFile(name: string, data: Text)
+      {
+  
+          console.log(data);
+            const linkSource = 'data:application/octet-stream;base64,' + data;
+            //const linkSource = data;
+            const downloadLink = document.createElement("a");
+            const fileName = name;
+  
+            downloadLink.href = linkSource;
+            downloadLink.download = fileName;
+            downloadLink.click();
+      }
 
 }
---
+++ src/app/home/giss/giss.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Giss } from "./giss";
+
+const API = "https://rpa.devplus.com.br";
+//const API = "https://localhost:7109";
+
+@Injectable({
+    providedIn:'root'
+})
+export class GissService {
+
+    constructor(private http: HttpClient) { }
+
+    listGiss() {
+        return this.http.get<Giss[]>(API + '/ConsultaGiss');
+
+    }
+    getDocument(id: number, typeDoc: string) {
+        return this.http.get<Text>(API + '/ConsultaGiss/Document/' + id + "?typeDoc=" + typeDoc);
+    }
+    reTry(id: number) {
+        return this.http.get(API + '/ConsultaGiss/Retry/' + id);
+    }
+    advancedReTry(id: number, instance: number) {
+        console.log('intance: ' + instance);
+          return this.http.get(API + '/ConsultaGiss/Retry/' + id + '/' + instance);
+    }
+
+}
