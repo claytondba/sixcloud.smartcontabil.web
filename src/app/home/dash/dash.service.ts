f86d40cf3d3c33eaa95bbaef944099e5d60b711b
claytonoliveira <clayton@devplus.com.br>
2023-09-22 07:43:39 -0300

    dash

--- src/app/home/dash/dash.service.ts
+++ src/app/home/dash/dash.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ServiceStatus } from "./models/service-status";
 
-const API = "https://rpa.devplus.com.br"
-//const API = "https://localhost:7109"
+const API = "https://rpa.devplus.com.br/api/v1"
+//const API = "https://localhost:7109/api/v1"
 
 @Injectable({
   providedIn:'root'
@@ -13,13 +13,13 @@ export class DashService {
   constructor(private http: HttpClient) { }
 
   statusCnd() {
-      return this.http.get<ServiceStatus[]>(API + '/Dashboard/cnd');
+      return this.http.get<ServiceStatus[]>(API + '/dashboard/cnd');
   }
   statusGinfes() {
-      return this.http.get<ServiceStatus[]>(API + '/Dashboard/ginfes');
+      return this.http.get<ServiceStatus[]>(API + '/dashboard/ginfes');
   }
   statusGiss() {
-    return this.http.get<ServiceStatus[]>(API + '/Dashboard/giss');
+    return this.http.get<ServiceStatus[]>(API + '/dashboard/giss');
   }
 
 }
